96eb9ffbd1e5699a9742afc7e182dc3455049aa4
van0014 <danvan1993@hotmail.com>
2026-07-05 10:55:55 +1000

    Update Song translate.bat

--- Song translate.bat
+++ Song translate.bat
@@ -1,13 +1,28 @@
 @echo off
 cd /D "%~dp0"
 
-:loop
-if "%~1"=="" goto end
+:: 1. If no file is dragged and dropped, go to the folder traversal block
+if "%~1"=="" goto no_input
 
-python song-translate.py --infile "%~1"
+:: 2. Process all dragged-and-dropped files
+for %%I in (%*) do (
+    echo "%%~nxI" | findstr /I "_ZH-HANS" >nul && (
+        echo Skipping already translated file: %%~nxI
+    ) || (
+        python song-translate.py --infile "%%~fI"
+    )
+)
+goto end
 
-shift
-goto loop
+:no_input
+echo No input file provided. Processing all .txt files in directory...
+for %%F in (*.txt) do (
+    echo "%%~nxF" | findstr /I "_ZH-HANS" >nul && (
+        rem Do nothing, this file is skipped
+    ) || (
+        python song-translate.py --infile "%%~fF"
+    )
+)
 
 :end
 pause
